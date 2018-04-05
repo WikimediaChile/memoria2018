4bec8551f33c9aad64c5de47b04872ce2398b837
superzerocool <dennis.tobar@gmail.com>
2018-04-05 12:50:50 -0300

    Ajustando el pie de página

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,34 +2,30 @@
 <section class="bg-dark text-white">
     <div class="container text-center">
         <div class="row">
-            <div class="col-sm-10 mr-auto text-left">
+            <div class="col-sm-12 mr-auto text-center">
+                <p>
                 <address>
                 <strong>Wikimedia Chile</strong><br />
                 Avenida Nueva Providencia 1881, oficina 2110<br />
                 Providencia, Santiago<br />
                 contacto@wikimedia.cl
               </address>
+                </p>
+                <p>
+                    <a href="https://twitter.com/wikimedia_cl" class="btn btn-outline-light">
+                    <i class="fa fa-twitter sr-contact"></i>
+                    </a> /
+                    <a href="https://facebook.com/wikimediaCL" class="btn btn-outline-light">
+                        <i class="fa fa-facebook"></i>
+                    </a> /
+                    <a href="https://wikimedia.cl" class="btn btn-outline-light">
+                        <i class="fa fa-globe"></i>
+                    </a> /
+                    <a href="https://meta.wikimedia.org/wiki/Wikimedia_Chile" class="btn btn-outline-light">
+                        <i class="fa fa-wikipedia-w"></i>
+                    </a>
+                </p>
             </div>
-            <div class="col-sm-2 mr-auto text-center">
-                <a href="https://twitter.com/wikimedia_cl" class="btn btn-outline-light">
-                    <i class="fa fa-twitter sr-contact"></i> Twitter
-                </a>
-                <br />
-                <a href="https://facebook.com/wikimediaCL" class="btn btn-outline-light">
-                    <i class="fa fa-facebook"></i> Facebook
-                </a>
-                <br />
-                <a href="https://wikimedia.cl" class="btn btn-outline-light">
-                    <i class="fa fa-globe"></i> Web oficial
-                </a>
-                <br />
-                <a href="https://meta.wikimedia.org/wiki/Wikimedia_Chile" class="btn btn-outline-light">
-                    <i class="fa fa-wikipedia-w"></i> Wikimedia
-                </a>
-                <br />
-
-            </div>
-
         </div>
         <div class="row">
             <div class="col-12 small">
